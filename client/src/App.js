f086b97cd8c6d5dbaf30fd84f924338587b299dd
Jonah <jonahpoulson@gmail.com>
2021-07-05 15:18:23 -0400

    spotted data/jsx/map bug, header and courses component built in

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,34 @@
 import React, {useState, useEffect} from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import axios from "axios";
 import './reset.css';
 import './App.css';
 
+// Components
+import Header from './components/Header';
+import Courses from './components/Courses';
+
+
 const App = () => {
-const [courses, setCourses] = useState();
+	const [courses, setCourses] = useState();
 
-useEffect(()=>{
-  axios.get("http://localhost:5000/api/courses")
-    .then(data => setCourses(data.data))
-}, [])
+	useEffect(()=>{
+		axios.get("http://localhost:5000/api/courses")
+			.then(data => setCourses(data.data))
+	}, [ ])
 
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<Courses courses={courses}/>
+
+			{/* <BrowserRouter>
+				<Switch>
+					<Route exact path="/" component={} />
+				</Switch>
+			</BrowserRouter> */}
+		</div>
+	);
 }
 
 export default App;
