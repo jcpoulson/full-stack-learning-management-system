c099ddab9f141da50dce1ecec04862e093948c5e
Jonah <jonahpoulson@gmail.com>
2021-07-08 15:37:41 -0400

    fixed post request bug in SignUp, also finished up component. Come back to this component after log in feature is built

--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -1,16 +1,15 @@
-// Stateful Component
-
 import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import axios from 'axios';
 
-const UserSignUp = () => {
+const UserSignUp = (props) => {
     const [firstName, setfirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [emailAddress, setEmailAddress] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('')
 
-
+    // This method updates state when the form changes
     const change = (event) => {
         if (event.target.id === "firstName") {
             setfirstName(event.target.value);
@@ -25,23 +24,45 @@ const UserSignUp = () => {
         }
     }
 
-    const submit = () => { // This submit function works just fine, it's just that the API returns a 400 status code
+    // This method sets up the post request to the API
+    const submit = () => {
+        // checks to see if password fields match 
         if (password !== confirmPassword) {
             console.log("Passwords do not match");
         }
 
-        const newUser = {firstName: firstName, lastName: lastName, emailAddress: emailAddress, password: password}
-        console.log(JSON.stringify(newUser));
-        
-        axios.post('http://localhost:5000/api/users', JSON.stringify(newUser))
-            .then(response => console.log(response))
+        // setting up the post request with axios
+        let data = JSON.stringify({
+            "firstName": firstName,
+            "lastName": lastName,
+            "emailAddress": emailAddress,
+            "password": password
+          });
+          
+          let config = {
+            method: 'post',
+            url: 'http://localhost:5000/api/users',
+            headers: { 
+              'Content-Type': 'application/json'
+            },
+            data : data
+          };
+          
+          axios(config)
+          .then(function (response) {
+            console.log(JSON.stringify(response.data));
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+          props.history.push('/')
     }
 
     return (
         <main>
             <div className="form--centered">
                 <h2>Sign Up</h2>
-                
+                {console.log(props)}
                 <div>
                     <label for="firstName">First Name</label>
                     <input onChange={change} id="firstName" name="firstName" type="text"/>
@@ -55,7 +76,7 @@ const UserSignUp = () => {
                     <input onChange={change} id="confirmPassword" name="confirmPassword" type="password"/>
                     <button className="button" onClick={submit}>Sign Up</button><button className="button button-secondary" onClick="event.preventDefault();">Cancel</button>
                 </div>
-                <p>Already have a user account? Click here to <a href="sign-in.html">sign in</a>!</p>
+                <p>Already have a user account? Click here to <NavLink to="/"><a>sign in</a></NavLink>!</p>
             </div>
         </main>
     )
