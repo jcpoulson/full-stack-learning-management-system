83d84ee9397933d9a70e7419accfb8c404a32ed0
Jonah <jonahpoulson@gmail.com>
2021-08-03 16:22:50 -0400

    working on UserSignOut

--- client/src/components/UserSignOut.js
+++ client/src/components/UserSignOut.js
@@ -5,9 +5,11 @@ import Cookies from 'js-cookie';
 const UserSignOut = (props) => {
     const history = useHistory();
 
-    props.setUser({});
-    Cookies.remove('authenticatedUser');
-    history.push('/')
+    console.log("this should print to the console, to test that the component call works")
+
+    // props.setUser({});
+    // Cookies.remove('authenticatedUser');
+    // history.push('/')
 }
 
 export default UserSignOut;
